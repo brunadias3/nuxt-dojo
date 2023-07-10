7533d91655194d66ec2ebc476e72efa1a1912d88
Bruna <brunadias_30@hotmail.com>
2023-07-10 16:27:47 -0300

    adicionando paginas

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum sit amet consectetur adipsinct Lorem ipsum sit amet consectetur adipsinct
+        Lorem ipsum sit amet consectetur adipsinct</p>
+        <p>Lorem ipsum sit amet consectetur adipsinct Lorem ipsum sit amet consectetur adipsinct
+        Lorem ipsum sit amet consectetur adipsinct</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+
+</style>
