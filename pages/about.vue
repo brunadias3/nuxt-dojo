e44edf5349a5739903b11b325a4dcbdee6a444b1
Bruna <brunadias_30@hotmail.com>
2023-07-11 13:00:22 -0300

    nuxt links

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        
         <h2>About</h2>
         <p>Lorem ipsum sit amet consectetur adipsinct Lorem ipsum sit amet consectetur adipsinct
         Lorem ipsum sit amet consectetur adipsinct</p>
