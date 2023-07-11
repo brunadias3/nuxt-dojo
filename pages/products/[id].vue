564dc775c532de8509cd8e3746cb20d87eb539f3
Bruna <brunadias_30@hotmail.com>
2023-07-11 12:51:14 -0300

    route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore eligendi asperiores minus at non eum doloribus culpa, labore, a, rem placeat magnam fugiat voluptate facilis. Cupiditate quasi illum facere! Sapiente!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
