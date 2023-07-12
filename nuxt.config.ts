49d433c555f02b68038d9d2b78f87eb39d098647
Bruna <brunadias_30@hotmail.com>
2023-07-11 21:07:48 -0300

    server routes dynamic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,15 @@ export default defineNuxtConfig({
         head: {
             title: 'Nuxt Dojo',
             meta: [
-                {name: 'description', content: 'Everything about Nuxt 3'}
+                { name: 'description', content: 'Everything about Nuxt 3' }
 
             ],
             link: [
-                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
             ]
         }
+    },
+    runtimeConfig: {
+        currencyKey: process.env.CURRENCY_API_KEY
     }
 })
--- pages/about.vue
+++ pages/about.vue
@@ -1,26 +1,29 @@
 <template>
-    <div>
-        <h2>About</h2>
-        <p>Lorem ipsum sit amet consectetur adipsinct Lorem ipsum sit amet consectetur adipsinct
-        Lorem ipsum sit amet consectetur adipsinct</p>
-        <p>Lorem ipsum sit amet consectetur adipsinct Lorem ipsum sit amet consectetur adipsinct
-        Lorem ipsum sit amet consectetur adipsinct</p>
-    </div>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum sit amet consectetur adipsinct Lorem ipsum sit amet
+      consectetur adipsinct Lorem ipsum sit amet consectetur adipsinct
+    </p>
+    <p>
+      Lorem ipsum sit amet consectetur adipsinct Lorem ipsum sit amet
+      consectetur adipsinct Lorem ipsum sit amet consectetur adipsinct
+    </p>
+    {{ data }}
+  </div>
 </template>
 
 <script setup>
-
+const { data } = await useFetch("/api/currency/GBP");
 </script>
 
 <style scoped>
-
 h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
+  margin-bottom: 20px;
+  font-size: 36px;
 }
 
 p {
-    margin: 20px 0;
+  margin: 20px 0;
 }
-
 </style>
